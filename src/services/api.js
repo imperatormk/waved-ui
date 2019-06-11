e863236eeac5350947a2f9dc2ec99e1bf195df87
Darko <darko.simonovski@bestisoft.com>
2019-06-11 03:00:18 +0200

    Get tracks from API (wip)

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,12 @@
 import http from './http'
 
 export default {
+  getTracks(songId) {
+    return http.get(`/songs/${songId}`)
+      .then(resp => resp.data)
+  },
   processTracks(id, data) {
     return http.post(`/audio/${id}`, data)
+      .then(resp => resp.data)
   },
 }
